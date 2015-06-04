8565758ccd063da1221e2c5d0a747407296783ab
plancys <kamil.kalandyk@codewise.com>
2015-06-04 19:31:27 +0200

    add directory combobox

--- ui/widgets.py
+++ ui/widgets.py
@@ -1,5 +1,15 @@
-from ttk import Treeview
+from Tkinter import StringVar
+import os
+from ttk import Treeview, Combobox
 import re
+
+
+def generate_label(directory):
+    return re.split('/', directory.name)[-1] + " [" + directory.readable_size() + " ]"
+
+
+def remove_wrong_characters(text):
+    return ''.join([i if ord(i) < 128 else '' for i in text])
 
 
 class DirectoryTreeView(Treeview):
@@ -9,8 +19,7 @@ class DirectoryTreeView(Treeview):
 
     def build_view_deep(self, tree_id, tree, subdirectories):
         for directory in subdirectories:
-            print directory
-            directory_name = re.split('/', directory.name)[-1] + " [" + directory.readable_size() + " ]"
+            directory_name = generate_label(directory)
             current_tree_id = tree.insert(tree_id, 'end', text=directory_name)
             if (directory.sub_directories is not None):
                 self.build_view_deep(current_tree_id, tree, directory.sub_directories)
@@ -18,3 +27,11 @@ class DirectoryTreeView(Treeview):
     def build_view(self, directory_root, tree):
         root_view_id = tree.insert('', 0, 'directories', text=directory_root.name)
         self.build_view_deep(root_view_id, tree, directory_root.sub_directories)
+
+
+class SubdirectoriesCombobox(Combobox):
+    def __init__(self, root_view, root_directory):
+        self.root_value = StringVar()
+        Combobox.__init__(self, root_view, textvariable=self.root_value)
+        dirs = next(os.walk(root_directory))[1]
+        self['values'] = [remove_wrong_characters(text) for text in dirs]
